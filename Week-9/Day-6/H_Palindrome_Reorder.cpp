9ae2020087ba6096f83fba99a3af331c38debe17
mdmasudrana271 <mr7123619@gmail.com>
2024-05-23 21:33:00 +0600

    day 6 done

---
+++ Week-9/Day-6/H_Palindrome_Reorder.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b) * b)
+using namespace std;
+
+int main()
+{
+    // write c++ program code
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    return 0;
+}
