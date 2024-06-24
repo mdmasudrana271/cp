c724846cace07820122cda99ecdf20c54015376b
mdmasudrana271 <mr7123619@gmail.com>
2024-06-24 22:36:26 +0600

    day 3 done

---
+++ Week 13/Day-3/A_Alice_and_Books.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b) * b)
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    // write c++ program code
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        int mx = 0, mx2 = v[n - 1];
+        for (int i = 0; i < n - 1; i++)
+        {
+            mx = max(mx, v[i]);
+        }
+        cout << mx + mx2 << endl;
+    }
+    return 0;
+}
