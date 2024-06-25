8e8b4f65fca200e9890299c14a336fadd9df9de8
mdmasudrana271 <mr7123619@gmail.com>
2024-06-25 22:01:17 +0600

    day 4 done

---
+++ Week 13/Day-4/A_Sushi_for_Two.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b) * b)
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    // write c++ program code
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    vector<pair<int, int>> cnt;
+    int currlen = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (v[i] == v[i - 1])
+        {
+            currlen++;
+        }
+        else
+        {
+            cnt.push_back({currlen, v[i - 1]});
+            currlen = 1;
+        }
+    }
+    cnt.push_back({currlen, v[n - 1]});
+    int ans = 0;
+    for (int i = 1; i < cnt.size(); i++)
+    {
+        if (cnt[i].second != cnt[i - 1].second)
+        {
+            int mn = min(cnt[i].first, cnt[i - 1].first);
+            ans = max(ans, 2 * mn);
+        }
+    }
+    cout << ans << endl;
+
+    return 0;
+}
