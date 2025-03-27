d0a93fa10161d32cd35d93ea359436749925ea84
mdmasudrana271 <mr7123619@gmail.com>
2025-03-27 23:09:09 +0600

    daily solve done

---
+++ CP-2025/day-6-25-03-27/A_Upload_More_RAM.cpp
@@ -0,0 +1,45 @@
+/*
+Date: 27 March 2025
+Time: 23:04:47
+codeforces user_name: abdullahal304
+codechef user_name: mdabdullahal30
+Author: MASUD
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b) * b)
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    // write c++ program code
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        if (n == 1)
+        {
+            cout << 1 << endl;
+        }
+        else if (k == 1)
+        {
+            cout << n << endl;
+        }
+        else
+        {
+            cout << (n * k) - (k - 1) << endl;
+        }
+    }
+    return 0;
+}
---
+++ CP-2025/day-6-25-03-27/B_Normal_Problem.cpp
@@ -0,0 +1,50 @@
+/*
+Date: 27 March 2025
+Time: 09:28:10
+codeforces user_name: abdullahal304
+codechef user_name: mdabdullahal30
+Author: MASUD
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b) * b)
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    // write c++ program code
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        int n = s.size();
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (s[i] == 'p')
+            {
+                cout << 'q';
+            }
+            else if (s[i] == 'q')
+            {
+                cout << 'p';
+            }
+            else
+            {
+                cout << s[i];
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
