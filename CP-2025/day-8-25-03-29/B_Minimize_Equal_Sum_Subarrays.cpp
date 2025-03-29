f6d4beaa39250c7ebdad897d532046eac0bce5f9
mdmasudrana271 <mr7123619@gmail.com>
2025-03-29 23:33:19 +0600

    dailsy solve done

---
+++ CP-2025/day-8-25-03-29/B_Minimize_Equal_Sum_Subarrays.cpp
@@ -0,0 +1,43 @@
+/*
+Date: 29 March 2025
+Time: 02:21:12
+codeforces user_name: abdullahal304
+codechef user_name: mdabdullahal30
+Author: MASUD
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b) * b)
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    // write c++ program code
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        for (int i = 1; i < n; i++)
+        {
+            cout << v[i] << " ";
+        }
+        cout << v[0] << "\n";
+    }
+    return 0;
+}
