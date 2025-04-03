cc37c378e0cc2d59084cb084e7d0cc3b598ef32c
mdmasudrana271 <mr7123619@gmail.com>
2025-04-03 19:38:48 +0600

    daily solve done

---
+++ CP-2025/day-13-25-04-03/D_Balanced_Round.cpp
@@ -0,0 +1,53 @@
+/*
+Date: 03 April 2025
+Time: 07:45:52
+codeforces user_name: abdullahal304
+codechef user_name: mdabdullahal30
+Author: MASUD
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b) * b)
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    // write c++ program code
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        int ans = 1, cnt = 1;
+        for (int i = 1; i < n; i++)
+        {
+            if (v[i] - v[i - 1] > k)
+            {
+                cnt = 1;
+            }
+            else
+            {
+                cnt++;
+            }
+            ans = max(ans, cnt);
+        }
+        cout << n - ans << endl;
+    }
+    return 0;
+}
