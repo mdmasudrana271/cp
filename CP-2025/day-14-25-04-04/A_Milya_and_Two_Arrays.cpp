122092afaa08ba1050f28ebdb17343d4026d04bc
mdmasudrana271 <mr7123619@gmail.com>
2025-04-04 11:47:34 +0600

    daily solve done

---
+++ CP-2025/day-14-25-04-04/A_Milya_and_Two_Arrays.cpp
@@ -0,0 +1,56 @@
+/*
+Date: 04 April 2025
+Time: 10:19:36
+codeforces user_name: abdullahal304
+codechef user_name: mdabdullahal30
+Author: MASUD
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b) * b)
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    // write c++ program code
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n), b(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> b[i];
+        }
+        set<int> st1, st2;
+        for (int i = 0; i < n; i++)
+        {
+            st1.insert(a[i]);
+            st2.insert(b[i]);
+        }
+        if (st1.size() + st2.size() < 4)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
