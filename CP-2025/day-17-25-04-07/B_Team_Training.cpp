eec3b1594828d9eec32e6ff12fc856a272596c39
mdmasudrana271 <mr7123619@gmail.com>
2025-04-07 09:53:57 +0600

    daily solve dove

---
+++ CP-2025/day-17-25-04-07/B_Team_Training.cpp
@@ -0,0 +1,49 @@
+/*
+Date: 07 April 2025
+Time: 09:51:14
+codeforces user_name: abdullahal304
+codechef user_name: mdabdullahal30
+Author: MASUD
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b) * b)
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    // write c++ program code
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end(), greater<int>());
+        int ans = 0;
+        for (int i = 0, cnt = 1; i < n; i++, cnt++)
+        {
+            if (a[i] * cnt >= x)
+            {
+                ans++;
+                cnt = 0;
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
