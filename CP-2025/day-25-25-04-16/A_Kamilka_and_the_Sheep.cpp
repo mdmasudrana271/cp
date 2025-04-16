729855b1e2f8778347ba50ace76937ce4ff0a1f5
mdmasudrana271 <mr7123619@gmail.com>
2025-04-16 21:24:34 +0600

    daily solve dove

---
+++ CP-2025/day-25-25-04-16/A_Kamilka_and_the_Sheep.cpp
@@ -0,0 +1,40 @@
+/*
+Date: 16 April 2025
+Time: 21:11:33
+codeforces user_name: abdullahal304
+codechef user_name: mdabdullahal30
+Author: MASUD
+*/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define ll long long int
+#define mod 1000000007
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a / gcd(a, b) * b)
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main()
+{
+    // write c++ program code
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        cout << v[n - 1] - v[0] << endl;
+    }
+    return 0;
+}
